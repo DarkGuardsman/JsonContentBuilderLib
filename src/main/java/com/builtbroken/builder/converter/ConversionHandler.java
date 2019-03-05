1b0991afe072cda3f0cf461aa0f29d29520753ec
Robert WC Seifert <rseifert.phone@gmail.com>
2019-03-05 10:13:12 -0500

    Added way to register converters

--- src/main/java/com/builtbroken/builder/converter/ConversionHandler.java
+++ src/main/java/com/builtbroken/builder/converter/ConversionHandler.java
@@ -10,6 +10,7 @@ import java.util.Map;
  */
 public class ConversionHandler
 {
+
     private final Map<String, IJsonConverter> converters = new HashMap();
     private final Map<String, IJsonConverter> altNames = new HashMap();
 
@@ -72,5 +73,60 @@ public class ConversionHandler
             return parent.toJson(type, object);
         }
         return null; //TODO throw error
+    }
+
+    public void addConverter(IJsonConverter converter)
+    {
+        if (converter != null)
+        {
+            if (converter.getUniqueID() != null && !converter.getUniqueID().trim().isEmpty())
+            {
+                final String type = converter.getUniqueID().toLowerCase();
+                if (converters.containsKey(type))
+                {
+                    System.out.println(this + ": Warning, overriding existing converter " +
+                            "[" + converters.get(type) + "] " +
+                            "with [" + converter + "] for id " + type);
+                    //TODO throw error if strict mode, and use logger
+                }
+                converters.put(type, converter);
+
+
+                if (converter.getAlias() != null)
+                {
+                    for (String string : converter.getAlias())
+                    {
+                        if (string != null && !string.trim().isEmpty())
+                        {
+                            if (altNames.containsKey(string.toLowerCase()))
+                            {
+                                throw new IllegalArgumentException(this + ": alias[" + string + "] is already in use by another converter " +
+                                        "Current: " + altNames.get(string.toLowerCase())
+                                        + " New: " + converter);
+                            }
+                            altNames.put(string.toLowerCase(), converter);
+                        }
+                        else
+                        {
+                            throw new IllegalArgumentException(this + ": invalid alias[" + string + "] for converter " + converter);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new IllegalArgumentException(this + ": converters require unique IDs to be set");
+            }
+        }
+        else
+        {
+            throw new IllegalArgumentException(this + ": Can not add a null converter");
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ConversionHandler[" + name + "]@" + hashCode();
     }
 }
